96bd9245bee4b193e091ccd0e8841527997123d5
assiayussiri659 <ayushdubey2053@gmail.com>
2024-07-10 18:27:01 +0530

    .js

--- src/context/CryptoContext.js
+++ src/context/CryptoContext.js
@@ -19,30 +19,15 @@ export const CryptoProvider = ({ children }) => {
   const [perPage, setPerPage] = useState(10);
 
 
-  // This is how you can do error handling by creating one state to store the error,
-  // This is only for example purpose and not covered in the video
-  // create one state for the error
+  
   const [error, setError] = useState({ data: "", coinData: "", search: "" });
-// there can be 3 errors that we can catch from all three functions, also send the error state 
-// through value prop
+
 
   const getCryptoData = async () => {
-    //here we will set an empty string for the data error
     setError({ ...error, data: "" });
     setCryptoData();
     setTotalPages(13220);
-    // try {
-    //   const data = await fetch(
-    //     `https://api.coingecko.com/api/v3/coins/list`
-    //   )
-    //     .then((res) => res.json())
-    //     .then((json) => json);
-
-    //   console.log(data);
-    //   setTotalPages(data.length);
-    // } catch (error) {
-    //   console.log(error);
-    // }
+    
 
     try {
       const data = await fetch(
@@ -52,13 +37,10 @@ export const CryptoProvider = ({ children }) => {
           return res.json();
         }
         let errorResponse = await res.json();
-        // here we might get the error so it is best to handle it and throw the error
-        // console.log(errorResponse);
         setError({ ...error, data: errorResponse.error });
         throw new Error(errorResponse.error);
       }).then((json) => json);
 
-      // console.log(data);
       setCryptoData(data);
     } catch (error) {
       console.log(error);
